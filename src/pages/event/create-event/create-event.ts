048b7155deaba9000349f3e7b318be9a677e9e59
radityasurya <radityasurya@hotmail.com>
2016-12-13 02:12:01 +0100

    add create event form

--- src/pages/event/create-event/create-event.ts
+++ src/pages/event/create-event/create-event.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavParams, ViewController } from 'ionic-angular';
+import { Validators, FormBuilder, FormControl } from '@angular/forms';
 
 import { EventService } from '../../../providers/event-service';
 
@@ -7,22 +8,37 @@ import { EventService } from '../../../providers/event-service';
   templateUrl: 'create-event.html',
   providers: [EventService]
 })
-export class CreateEventPage  {
- 
+
+export class CreateEventPage {
+  background_image: string;
+  event: any;
+
   constructor(public params: NavParams,
               public viewCtrl: ViewController,
-              public eventService: EventService) {
-    this.eventService.create()
-      .subscribe(
-        data => {
-          console.log(data);
-        }, err => {
-          console.log(err._body);
-        }
-      );
+              public eventService: EventService,
+              public formBuilder: FormBuilder,) {
+    this.background_image = 'assets/img/create-event-big.jpg';
+
+    this.event = this.formBuilder.group({
+      'name': ['', Validators.required],
+      'email': ['', Validators.required],
+      'password': ['', Validators.required],
+      'confirm_password': ['', Validators.required]
+    });
   }
-  
+
   dismiss() {
     this.viewCtrl.dismiss();
+  }
+
+  create(): void {
+    this.eventService.create()
+      .subscribe(
+      data => {
+        console.log(data);
+      }, err => {
+        console.log(err._body);
+      }
+      );
   }
 }
