2b19b4ed4022310715371450a6d001b85d82eda4
radityasurya <radityasurya@hotmail.com>
2017-01-08 23:54:21 +0100

    finished profile bio

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,5 +1,5 @@
-import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
 
 import { User } from '../../models/user';
 
@@ -17,8 +17,11 @@ import { ProfileService } from '../../providers/profile-service';
 })
 export class ProfilePage {
   user: User;
+  tabs: any;
+  events: any;
 
   constructor(public navCtrl: NavController, private profileService: ProfileService) {
+    this.tabs = "bio";
   }
 
   ngOnInit() {
@@ -27,11 +30,27 @@ export class ProfilePage {
         this.user = data['user'];
         this.user.profile_pic = 'https://api.adorable.io/avatars/120/default';
         this.user.cover_pic = "https://images.unsplash.com/photo-1481204287293-36160be24f29";
-        console.log(this.user);
-      });
+        this.user.bio = "Travel writer by profession and lover of world cultures, languages, souls, food, oceans, wild spaces and urban places by nature.";
+        this.user.city = "Eindhoven";
+        this.user.nationality = "Dutch";
+        this.user.phone = "1-800-Hotline-Bling";
 
+        console.log(this.user);
+
+        this.profileService.loadMyEvent(this.user.id)
+        .subscribe(data => {
+          console.log(data);
+          this.events = data;
+        });
+      });
   }
   ionViewDidLoad() {
+  }
+
+  loadPicture(category): string {
+    let imgUrl = 'assets/img/category/' + category + '.jpg';
+    console.log(category);
+    return imgUrl;
   }
 
 }
